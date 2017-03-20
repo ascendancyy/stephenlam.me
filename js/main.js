af2f329ecadbaaa926ecef954e0b9b36fa8e8c03
Stephen Lam <ascendancy.sl@gmail.com>
2017-03-19 21:44:16 -0700

    Remove Centry Gothic and increase font load timeout

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ const book = new FontFaceObserver('FuturaBT-Book')
 const medium = new FontFaceObserver('FuturaBT-Medium')
 const bold = new FontFaceObserver('FuturaBT-Bold')
 
-Promise.all([book.load(), medium.load(), bold.load()])
+const timeout = 5500
+
+Promise.all([book.load(null, timeout), medium.load(null, timeout), bold.load(null, timeout)])
   .then(() => document.documentElement.classList.add('fonts-loaded'))
+  .catch(() => console.log('Font did not load after 5 seconds'))
