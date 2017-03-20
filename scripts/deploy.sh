3a0fe8c92a499808f334da84c25a6d670050c917
Stephen Lam <ascendancy.sl@gmail.com>
2017-03-19 21:44:39 -0700

    Change deploy script to allow deploy to specific site

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,8 +11,13 @@ if [ -n "$TRAVIS_BUILD_ID" ]; then
     rsync --delete --include=img/projects/ --exclude=projects/ -avH ${TRAVIS_BUILD_DIR}/_site/ -e ssh ${SSH_USER}@stephenlam.me:${DEPLOY_DIR}/${SITE}
 else
     if [ -n "$1" ] &&  [ -n "$2" ]; then
-        echo "Deploying to ${2}/${SITE} as ${1}"
-        rsync --delete --include=img/projects/ --exclude=projects/ -avH ./_site/ -e ssh ${1}@stephenlam.me:${2}/${SITE}
+        if [ -n "$3" ]; then
+            echo "Deploying to ${2}/${3} as ${1}"
+            rsync --delete --include=img/projects/ --exclude=projects/ -avH ./_site/ -e ssh ${1}@stephenlam.me:${2}/${3}
+        else
+            echo "Deploying to ${2}/${SITE} as ${1}"
+            rsync --delete --include=img/projects/ --exclude=projects/ -avH ./_site/ -e ssh ${1}@stephenlam.me:${2}/${SITE}
+        fi
     else
         echo "Enter ssh user and site path."
     fi
