673752a50d149a5acaa68db8aedb864ef8a3f12b
Stephen Lam <ascendancy.sl@gmail.com>
2017-07-30 21:06:14 -0700

    Fix improper import

--- src/js/prep.js
+++ src/js/prep.js
@@ -1,9 +1,9 @@
-import { getRandomColor } from 'src/js/color';
+import { getFlowColor } from 'src/js/color';
 import { random } from 'src/js/util';
 
 window.addEventListener('DOMContentLoaded', () => {
   Array.from(document.getElementsByClassName('block')).forEach(block => {
-    block.style.backgroundColor = getRandomColor(random(-5, 5), 1);
+    block.style.backgroundColor = getFlowColor(random(-5, 5), 1);
     block.addEventListener('animationend', function blocksHidden () {
       block.parentElement.removeChild(block);
     }, { once: true });
