5fd8da75fccc2196017874fad05cd6c29ebaae1e
Stephen Lam <ascendancy.sl@gmail.com>
2018-02-03 02:11:16 -0800

    fix: analytics init

--- src/js/main.js
+++ src/js/main.js
@@ -15,7 +15,7 @@ const PRODUCTION = process.env.NODE_ENV === 'production';
 
 if (PRODUCTION) {
   import(/* webpackChunkName: "ga" */ 'src/js/ga')
-    .then(analytics => analytics.init());
+    .then(analytics => analytics.default());
 }
 
 if (PRODUCTION) {
